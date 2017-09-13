ff735bb4ce3fa2fcb5d23b5f2c0c9eb9adccc118
Nicolas Renaud <n.renaud@tudelft.nl>
2017-09-13 15:51:18 +0200

    small fix

--- map/gendatatool.py
+++ map/gendatatool.py
@@ -1,10 +1,9 @@
 import numpy as np 
 import os
-from gridtool import GridTools 
 import subprocess as sp 
 import sys
 
-
+from deeprank.map import gridtool as gt
 
 '''
 	Generate the input/output data on the grids for a series of prot-prot conformations
@@ -20,10 +19,13 @@ import sys
 	grid info
 
 			dictionay containing the grid information
-			see below for details
+			see gridtool.py for details
+
+	reset
+			Boolean to force the removal of all data
 '''
 
-def generate_data(data_folder, grid_info,data_type='haddock'):
+def map_features(data_folder, grid_info,data_type='haddock',reset=False):
 
 
 	# check all the input PDB files
@@ -31,7 +33,7 @@ def generate_data(data_folder, grid_info,data_type='haddock'):
 	sub_names = sub_names.split()
 
 	# copy the cuve generating file in the folder
-	sp.call('cp generate_cube_file.py %s' %folder)
+	#sp.call('cp generate_cube_file.py %s' %data_folder)
 
 
 	# loop over the data files
@@ -40,12 +42,12 @@ def generate_data(data_folder, grid_info,data_type='haddock'):
 		# molecule name we want
 		sub = sub_.decode('utf-8')
 
-		# the path to store the data
-		if os.path.isdir(sub+'/input'):
+		# remove the data if we wnat to force that
+		if os.path.isdir(sub+'/input') and reset:
 			os.system('rm -rf %s' %(sub+'/input'))
 
-		#if not os.path.isdir(sub+'/input'):
-		os.mkdir(sub+'/input/')
+		if not os.path.isdir(sub+'/input'):
+			os.mkdir(sub+'/input/')
 		
 
 		# molecule name
@@ -61,7 +63,7 @@ def generate_data(data_folder, grid_info,data_type='haddock'):
 			res_feat = None
 
 		# compute the data we want on the grid
-		grid = GridTools(mol_name=mol_name,
+		grid = gt.GridTools(mol_name=mol_name,
 						 data_type = data_type,
 			             number_of_points = grid_info['number_of_points'],
 			             resolution = grid_info['resolution'],
@@ -85,4 +87,4 @@ if __name__ == "__main__":
 	folder ='/home/nico/Documents/projects/deeprank/deeprank_classifier/test_set/'
 
 	# go
-	generate_data(folder,grid_info)
+	map_features(folder,grid_info)
