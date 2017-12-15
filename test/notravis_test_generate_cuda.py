54af1e26d2094c524fdcca47779450e2a135cc15
Nicolas Renaud <n.renaud@tudelft.nl>
2017-12-15 10:13:01 +0100

    gpu grid test

--- test/notravis_test_generate_cuda.py
+++ test/notravis_test_generate_cuda.py
@@ -1,7 +1,7 @@
 import sys
 import deeprank.generate
 
-def test_generate():
+def test_generate(tune,test,gpu_block):
 
 	# sources to assemble the data base
 	pdb_source     = ['./1AK4/decoys/']
@@ -28,13 +28,25 @@ def test_generate():
 	}
 
 	# tune the kernel 
-	#database.tune_cuda_kernel(grid_info)
-	database.test_cuda(grid_info,[8,8,8])
-	# map the features
-	#database.map_features(grid_info,cuda=True,gpu_block=[2,32,8])
+	if tune:
+		database.tune_cuda_kernel(grid_info)
+	elif test:
+		database.test_cuda(grid_info,gpu_block)
+	else:
+		database.map_features(grid_info,cuda=True,gpu_block=gpu_block)
 
 
 if __name__ == "__main__":
-	test_generate()
+
+	import argparse
+
+	parser = argparse.ArgumentParser(description='Map data using CUDA')
+	parser.add_argument('--tune', action='store_true',help="tune the kernel")
+	parser.add_argument('--test',action='store_true',help='test the kernel on 1 map')
+	parser.add_argument('--gpu_block',nargs='+',default=[8,8,8],type=int,help='number of gpu block to use. Default: 8 8 1')
+	args = parser.parse_args()
+
+
+	test_generate(args.tune,args.test,args.gpu_block)
 
 
