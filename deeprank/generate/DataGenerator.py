956840b3e4dd079b5a503c5671059b3a66d79e80
Nicolas Renaud <n.renaud@tudelft.nl>
2017-12-21 17:29:22 +0100

    fix

--- deeprank/generate/DataGenerator.py
+++ deeprank/generate/DataGenerator.py
@@ -265,7 +265,7 @@ class DataGenerator(object):
 		# remove the data where we had issues
 		if remove_error:
 			for mol in self.feature_error:
-				print('removing %s from %f' %(mol,self.hdf5))
+				print('removing %s from %s' %(mol,self.hdf5))
 				del self.f5[mol]
 
 		# close the file
@@ -569,7 +569,7 @@ class DataGenerator(object):
 		# remove the molecule with issues
 		if remove_error:
 			for mol in self.map_error:
-				print('removing %s from %f' %(mol,self.hdf5))
+				print('removing %s from %s' %(mol,self.hdf5))
 				del f5[mol]
 
 		# close he hdf5 file
