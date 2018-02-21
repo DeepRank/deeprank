edce0b4cfb5bb9b39cbcf416885354673135e96a
Nicolas Renaud <n.renaud@tudelft.nl>
2018-02-21 14:20:37 +0100

    revert test

--- test/notravis_test_learn.py
+++ test/notravis_test_learn.py
@@ -14,13 +14,14 @@ def test_learn():
 
   # declare the dataset instance
   data_set = DataSet(database,
-                            test_database = database,
+                            test_database = None,
+                            grid_shape=(30,30,30),
                             select_feature={'AtomicDensities_ind' : 'all',
                                             'Feature_ind' : ['coulomb','vdwaals','charge','pssm'] },
                             select_target='IRMSD',tqdm=True,
                             normalize_features = True, normalize_targets=True)
                             #pair_chain_feature=np.add,
-                            #dict_filter={'DOCKQ':[0.,0.6]})
+                            #dict_filter={'IRMSD':'<4. or >10.'})
 
 
   # create the network
@@ -28,7 +29,7 @@ def test_learn():
                     cuda=False,plot=True,outdir='./out/')
 
   # start the training
-  model.train(nepoch = 50,divide_trainset=0.8, train_batch_size = 5,num_workers=0)
+  model.train(nepoch = 1,divide_trainset=0.8, train_batch_size = 5,num_workers=0)
 
   # save the model
   model.save_model()
