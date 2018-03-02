dbb2f8e7f2d63dc2f7b745046eeef0d203508e05
Nicolas Renaud <n.renaud@tudelft.nl>
2018-03-02 16:12:57 +0100

    please

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../deeprank/'))
+sys.path.insert(0, os.path.abspath('../'))
 # sys.path.insert(0, os.path.abspath('.'))
 # sys.path.insert(0, os.path.abspath('..'))
 #sys.path.append(os.path.dirname(__file__))
