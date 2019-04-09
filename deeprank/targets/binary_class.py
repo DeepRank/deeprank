24ec7a484f68601ea30eebf90e19a4e948719e44
LilySnow <me.lixue@gmail.com>
2019-04-09 14:40:43 +0200

    binary_class.py now gives targets based on irmsd

--- deeprank/targets/binary_class.py
+++ deeprank/targets/binary_class.py
@@ -1,24 +1,31 @@
 import numpy as np
 
-def __compute_target__(decoy,targrp):
+def __compute_target__(decoy, targrp):
+    """
+        pdb_data (bytes): PDB translated in bytes
+        targrp (h5 file handle): name of the group where to store the targets
+
+        e.g.,
+        f = h5py.File('1LFD.hdf5')
+        targrp = f['1LFD_9w/targets']
+
+        list(targrp)
+        ['DOCKQ', 'FNAT', 'IRMSD', 'LRMSD']
+    """
 
     # fet the mol group
     molgrp = targrp.parent
     molname = molgrp.name
 
-    if 'BIN_CLASS' in targrp.keys():
-        del targrp['BIN_CLASS']
+    for target_name in ['BIN_CLASS']:
+        if target_name in targrp.keys():
+            del targrp[target_name]
 
-    if 'DOCKQ' in targrp.keys():
-        if targrp['DOCKQ'][()] == 1.0:
-            targrp.create_dataset('BIN_CLASS',data=np.array(1.0))
-        else:
-            targrp.create_dataset('BIN_CLASS',data=np.array(0.0))
+    if targrp['IRMSD'].value <=4:
+        print (f"This is a hit (irmsd <=4A). {molname} -> irmsd: {targrp['IRMSD'].value}")
+        classID = 1
     else:
-        # if we have a ref
-        if '_' not in molname:
-            targrp.create_dataset('BIN_CLASS',data=np.array(1.0))
+        classID = 0
 
-        # or it's a decoy
-        else:
-            targrp.create_dataset('BIN_CLASS',data=np.array(0.0))
+    targrp.create_dataset('BIN_CLASS',data=np.array(classID))
+
