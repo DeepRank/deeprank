7593b3079c59f79c9e6375bd27b73a92effb1cee
LilySnow <me.lixue@gmail.com>
2019-04-12 09:53:58 +0200

    updated plot_hitrate.py

--- deeprank/utils/plot_hitrate.py
+++ deeprank/utils/plot_hitrate.py
@@ -8,6 +8,64 @@ import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pandas as pd
+from ggplot import *
+
+def plot_boxplot_todo(hdf5,epoch=None,figname=None,inverse = False):
+
+    '''
+    Plot a boxplot of predictions VS targets useful '
+    to visualize the performance of the training algorithm
+    This is only usefull in classification tasks
+
+    Args:
+        figname (str): filename
+
+    '''
+
+    print('\n --> Box Plot : ', figname, '\n')
+
+    color_plot = {'train':'red','valid':'blue','test':'green'}
+    labels = ['train','valid','test']
+
+    #-- read data
+    h5 = h5py.File(hdf5,'r')
+    if epoch is None:
+        keys = list(h5.keys())
+        last_epoch_key = list(filter(lambda x: 'epoch_' in x,keys))[-1]
+    else:
+        last_epoch_key = 'epoch_%04d' %epoch
+        if last_epoch_key not in h5:
+            print('Incorrect epcoh name\n Possible options are: ' + ' '.join(list(h5.keys())))
+            h5.close()
+            return
+    h5data = h5[last_epoch_key]
+
+    print(f"Generate boxplot for {last_epoch_key} epoch ...")
+
+
+    n_panels = len(labels)
+    data = pd.DataFrame()
+
+    for l in labels:
+
+        if l in h5data:
+
+            tar = h5data[l]['targets']
+            raw_out = h5data[l]['outputs']
+
+            num_hits = list(tar.value).count(1)
+            total_num=len(tar)
+            print(f"According to 'targets' -> num of hits for {l}: {num_hits} out of {len(tar.value)}")
+            m = nn.Softmax(dim = 0)
+            final_out = np.array(m(torch.FloatTensor(raw_out)))
+            data_df = pd.DataFrame(list(zip([l]*total_num,raw_out,tar,final_out[:,1])), columns = ['label', 'raw_out', 'target', 'prediction'])
+            data = pd.concat([data, data_df] )
+    print(data)
+    p= ggplot(aes(x = "target", y = "prediction"), data=data) +geom_boxplot() + facet_grid(None, "label")
+    p.save(figname)
+
+
 
 
 def plot_boxplot(hdf5,epoch=None,figname=None,inverse = False):
@@ -52,20 +110,17 @@ def plot_boxplot(hdf5,epoch=None,figname=None,inverse = False):
 
         if l in h5data:
 
-            tar = h5data[l]['targets']
-            out = h5data[l]['outputs']
+            tar = h5data[l]['targets'].value
+            out = h5data[l]['outputs'].value
 
-            num_hits = list(tar.value).count(1)
-            print(f"According to 'targets' -> num of hits for {l}: {num_hits} out of {len(tar.value)}")
-#            m = nn.Softmax()
-#            r = m(torch.FloatTensor(out))
-#            print(f"r:{r}")
+            num_hits = list(tar).count(1)
+            print(f"According to 'targets' -> num of hits for {l}: {num_hits} out of {len(tar)}")
 
             data = [[], []]
             for pts,t in zip(out,tar):
                 r = F.softmax(torch.FloatTensor(pts), dim=0).data.numpy()
-    #            print(f"prediction: {pts}; target: {t}; r: {r}")
-                data[t].append(r[t])
+                #print(f"prediction: {pts}; target: {t}; r: {r}")
+                data[t].append(r[1])
 
             ax[0, iwin].boxplot(data)
             ax[0, iwin].set_xlabel(l)
