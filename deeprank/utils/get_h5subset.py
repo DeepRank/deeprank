7b6c0f5e9134f342379a61a4c59735d070d33005
LilySnow <me.lixue@gmail.com>
2019-07-16 11:15:09 +0200

    rename get_subset.py get_h5subset.py

---
+++ deeprank/utils/get_h5subset.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# Li Xue
+#  2-May-2019 11:24
+#
+# Extract five molecules from each hdf5 file and write into a new h5 file.
+#
+# This script is used to generate a small set for debugging.
+
+import sys
+import re
+import h5py
+import os
+
+h5FL = sys.argv[1]#'001_1GPW.hdf5'
+outDIR = sys.argv[2] # '.../'
+
+filename = os.path.basename(h5FL)
+new_h5FL = outDIR + filename
+
+f_in = h5py.File(h5FL, 'r')
+f_out = h5py.File(new_h5FL,'w')
+modelIDs = list(f_in)
+modelIDs
+
+for x in modelIDs[0:5]:
+    print(x)
+    f_in.copy(f_in[x],f_out)
+list(f_out)
+f_in.close()
+f_out.close()
+
+print(f"{new_h5FL} generated.")
+
