e7364ad209abf069043a9f1f123f92881e838d4f
Cunliang Geng <c.geng@esciencecenter.nl>
2019-09-11 11:41:08 +0200

    remove trailing whitespace

--- deeprank/config/__init__.py
+++ deeprank/config/__init__.py
@@ -2,7 +2,7 @@ import logging
 import logging.config
 
 from . import logger_settings
-from .chemicals import AA_codes, AA_codes_3to1, AA_codes_1to3 
+from .chemicals import AA_codes, AA_codes_3to1, AA_codes_1to3
 from .chemicals import AA_codes_pssm_ordered
 from .chemicals import AA_properties
 
