dc551745d3295edd4b715e6b1798643c3ce0605f
Cunliang Geng <c.geng@esciencecenter.nl>
2019-09-12 17:14:35 +0200

    update chemicals

--- deeprank/config/__init__.py
+++ deeprank/config/__init__.py
@@ -5,6 +5,7 @@ from . import logger_settings
 from .chemicals import AA_codes, AA_codes_3to1, AA_codes_1to3 
 from .chemicals import AA_codes_pssm_ordered
 from .chemicals import AA_properties
+from .chemicals import atom_vdw_radius, atom_vdw_radius_noH
 
 # Debug
 DEBUG = False
