1d0b4fcce6deb4e75617f4aea87fc351ba0733e3
Cunliang Geng <c.geng@esciencecenter.nl>
2019-10-31 10:34:30 +0100

    set pdb2sql v0.2.1 in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         'matplotlib',
         'torchsummary',
         'torch',
-        'pdb2sql'
+        'pdb2sql >= 0.2.1',
         'freesasa==2.0.3.post7;platform_system=="Linux"',
         'freesasa==2.0.3.post6;platform_system=="Darwin"'
         ],
