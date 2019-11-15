dc773ad8d6c82320bb01c89efe44c2da90bcb03b
Cunliang Geng <c.geng@esciencecenter.nl>
2019-11-15 11:35:10 +0100

    update fnat, irmsd and lrmsd calculations in dockQ.py

--- deeprank/targets/dockQ.py
+++ deeprank/targets/dockQ.py
@@ -12,21 +12,25 @@ def __compute_target__(decoy, targrp):
     Args:
         decoy(bytes): pdb data of the decoy
         targrp(hdf5 file handle): HDF5 'targets' group
-
-    Examples:
-        >>> f = h5py.File('1LFD.hdf5')
-        >>> decoy = f['1LFD_9w/complex'][()]
-        >>> targrp = f['1LFD_9w/targets']
     """
-
     tarname = 'DOCKQ'
     if tarname in targrp.keys():
         del targrp[tarname]
         warnings.warn(f"Removed old {tarname} from {targrp.parent.name}")
 
-    irmsd = rmsd_fnat.__compute_target__(decoy, targrp, "IRMSD")
-    lrmsd = rmsd_fnat.__compute_target__(decoy, targrp, "LRMSD")
-    fnat = rmsd_fnat.__compute_target__(decoy, targrp, "FNAT")
+    tarelems = ['FNAT', 'LRMSD', 'IRMSD']
+    vals = {}
+    for tarelem in tarelems:
+        # if target element exist, then use its value; otherwise calculate it
+        if tarelem not in targrp:
+            _ = rmsd_fnat.__compute_target__(decoy, targrp, tarelem)
+        # empty dataset
+        elif targrp[tarelem][()].shape is None:
+            _ = rmsd_fnat.__compute_target__(decoy, targrp, tarelem)
 
-    dockQ = StructureSimilarity.compute_DockQScore(fnat, lrmsd, irmsd)
+        # get the value
+        vals[tarelem] = targrp[tarelem][()]
+
+    dockQ = StructureSimilarity.compute_DockQScore(
+            vals['FNAT'], vals['LRMSD'], vals['IRMSD'])
     targrp.create_dataset(tarname, data=np.array(dockQ))
