e9478c5a8f8bce7653a40591c910be0296c3173a
Coos Baakman <coos.baakman@radboudumc.nl>
2021-03-18 14:21:57 +0000

    created a parser for the top file (containing charges)

---
+++ test/parse/test_top.py
@@ -0,0 +1,17 @@
+from nose.tools import eq_, ok_
+
+from deeprank.parse.top import TopParser
+
+
+_top_path = "deeprank/features/forcefield/protein-allhdg5-4_new.top"
+
+
+def test_parse():
+    with open(_top_path, 'rt') as f:
+        result = TopParser.parse(f)
+
+    eq_(len(result), 705)
+
+    for obj in result:
+        eq_(type(obj.kwargs['type']), str)
+        eq_(type(obj.kwargs['charge']), float)
