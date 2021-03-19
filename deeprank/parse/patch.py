790d985304838c1af1d8f2f38f85ba82381f0f78
Coos Baakman <coos.baakman@radboudumc.nl>
2021-03-19 08:29:29 +0000

    moved the enums for use in other classes

--- deeprank/parse/patch.py
+++ deeprank/parse/patch.py
@@ -1,21 +1,7 @@
 import re
 from enum import Enum
 
-
-class PatchActionType(Enum):
-    MODIFY = 1
-    ADD = 2
-
-
-class PatchResidueSelectionType(Enum):
-    NTER = 1
-    PROP = 2
-    CTER = 3
-    CTN = 4
-    DISU = 5
-    CYNH = 6
-    HISE = 7
-    HISD = 8
+from deeprank.models.patch import PatchActionType, PatchResidueSelectionType
 
 
 class PatchSelection:
