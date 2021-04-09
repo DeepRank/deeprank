c4b859c9ba76741c481a5d0bd503e2a9c2b55e64
Coos Baakman <coos.baakman@radboudumc.nl>
2021-04-09 08:00:57 +0000

    more extensively test the output of the grid computation

--- test/generate/test_gridtools.py
+++ test/generate/test_gridtools.py
@@ -15,7 +15,36 @@ from deeprank.models.mutant import PdbMutantSelection
 _log = logging.getLogger(__name__)
 
 
+def _get_feature_grid(hdf5, feature_group_name, feature_name, points_count):
+
+    # Check that the feature grid exists:
+    assert feature_group_name in hdf5, \
+        "{} not in hdf5, candidates are: {}".format(",".join(hdf5.keys()))
+    assert feature_name in hdf5[feature_group_name], \
+        "{} not in feature group, candidates are: {}".format(feature_name, ",".join(hdf5[feature_group_name].keys()))
+
+    # Check the grid size
+    eq_(hdf5[feature_group_name][feature_name].attrs['sparse'], False)
+    eq_(hdf5[feature_group_name][feature_name]['value'].shape, (points_count, points_count, points_count))
+
+    return hdf5[feature_group_name][feature_name]['value']
+
+
+def gt_(value1, value2):
+    assert value1 > value2, "{} <= {}".format(value1, value2)
+
+
+def lt_(value1, value2):
+    assert value1 < value2, "{} >= {}".format(value1, value2)
+
+
 def test_feature_mapping():
+    """ In this test, we investigate a set of five atoms. We make the grid tool take
+        the atoms in a 20 A radius around the first atom and compute the carbon density grid around it.
+        We're also positioning a feature on a grid and investigate its resulting distribution.
+
+        Grid values should be high close to the set position and low everywhere else.
+    """
 
     pdb_name = "1XXX"
 
@@ -37,7 +66,8 @@ def test_feature_mapping():
                 "ATOM      1  C   XXX A   1       0.000   0.000   0.000  1.00  0.00      C   C\n",
                 "ATOM      2 CA   XXX A   2       1.000   1.000   1.000  1.00  0.00      C   C\n",
                 "ATOM      3  N   XXX A   2      -1.000  -1.000  -1.000  1.00  0.00      C   N\n",
-                "ATOM      4  N   XXX A   3      20.000  20.000  20.000  1.00  0.00      C   N\n",
+                "ATOM      4  N   XXX A   3      10.000  10.000  10.000  1.00  0.00      C   N\n",
+                "ATOM      5  C   XXX A   4     -10.000 -10.000 -10.000  1.00  0.00      C   C\n",
             ]
             data = numpy.array(atom_lines).astype('|S78')
             mol_group.create_dataset('complex', data=data)
@@ -48,7 +78,7 @@ def test_feature_mapping():
             chain_id = "A"
             chain_number = 0
             position = [10.0, 10.0, 10.0]  # this should fit inside the grid
-            value = 0.123
+            value = 0.923
 
             data = numpy.array([[chain_number] + position + [value]])
             feature_group.create_dataset(feature_type_name, data=data)
@@ -60,26 +90,32 @@ def test_feature_mapping():
                                   number_of_points=points_count, resolution=1.0,
                                   atomic_densities={'C': 1.7},  # only collect density data on carbon
                                   feature=[feature_type_name],
+                                  contact_distance=20.0,  # just take all the atoms, close and far
                                   try_sparse=False)
 
-            # Check that the test feature and carbon atomic density are mapped to the grid.
-            for feature_group_name in ["%s/mapped_features/AtomicDensities_ind" % pdb_name,
-                                       "%s/mapped_features/Feature_ind" % pdb_name]:
+            carbon_density_grid = _get_feature_grid(f5,
+                                                    "%s/mapped_features/AtomicDensities_ind" % pdb_name,
+                                                    "C_%s" % chain_id,
+                                                    points_count)
 
-                # Check that the feature group actually exists, before checking inside it.
-                ok_(feature_group_name in f5)
-                ok_(len(f5[feature_group_name]) > 0)
+            # Check that the gaussian is high at the carbon atom positions:
+            gt_(carbon_density_grid[14][14][14], 0.1)
+            gt_(carbon_density_grid[4][4][4], 0.1)
 
-                for feature_name in f5[feature_group_name]:
+            # Check that nitrogens are not participating:
+            lt_(carbon_density_grid[24][24][24], 0.001)
 
-                    _log.debug("check {}.{}".format(feature_group_name, feature_name))
+            feature_grid = _get_feature_grid(f5,
+                                             "%s/mapped_features/Feature_ind" % pdb_name,
+                                             "%s_chain000" % feature_type_name,
+                                             points_count)
 
-                    # The number of values should match the grid settings.
-                    eq_(f5[feature_group_name][feature_name].attrs['sparse'], False)
-                    eq_(f5[feature_group_name][feature_name]['value'].shape, (points_count, points_count, points_count))
+            # Check that the feature is high at the set position:
+            gt_(feature_grid[24][24][24], 0.1)
 
-                    # It should be nonzero at least at one grid point.
-                    ok_(numpy.sum(f5[feature_group_name][feature_name]['value']) > 0.0)
+            # Check that the feature is low where it was not set:
+            lt_(feature_grid[0][0][0], 0.001)
+
     finally:
         # Whether the test completes successfully or not, it needs to clean up after itself.
         shutil.rmtree(tmp_dir)
