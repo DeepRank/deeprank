1d5ac8024004fd4764b1abf630c622a74921bc5e
Coos Baakman <coos.baakman@radboudumc.nl>
2021-04-30 11:46:27 +0000

    add a parser for pssm

--- deeprank/models/residue.py
+++ deeprank/models/residue.py
@@ -1,18 +1,11 @@
 
 
 class Residue:
-    def __init__(self, number, name):
+    def __init__(self, number, name, chain_id):
         self.number = number
         self.name = name
+        self.chain_id = chain_id
         self.atoms = []
-
-    @property
-    def chain_id(self):
-        chain_ids = set([atom.chain_id for atom in self.atoms])
-        if len(chain_ids) > 1:
-            raise ValueError("residue {} {} contains atoms of different chains: {}".format(self.name, self.number, self.atoms))
-
-        return list(chain_ids)[0]
 
     def __hash__(self):
         return hash((self.chain_id, self.number))
@@ -21,4 +14,4 @@ class Residue:
         return self.chain_id == other.chain_id and self.number == other.number
 
     def __repr__(self):
-        return "Residue {} {}".format(self.name, self.number)
+        return "Residue {} {} in {}".format(self.name, self.number, self.chain_id)
---
+++ deeprank/parse/pssm.py
@@ -0,0 +1,34 @@
+from deeprank.models.residue import Residue
+from deeprank.models.pssm import Pssm
+from deeprank.config.chemicals import AA_codes_pssm_ordered, AA_codes_1to3, AA_codes_3to1
+
+
+def parse_old_pssm(file_):
+    pssm = Pssm()
+    for line in file_:
+        data = line.split()
+        chain_id, residue_number, residue_name = data[:3]
+        values = data[3:]
+        residue = Residue(residue_number, residue_name, chain_id)
+        for code, value in zip(AA_codes_pssm_ordered, values):
+            pssm.set_amino_acid_value(residue, code, float(value))
+    return pssm
+
+
+def parse_new_pssm(file_, chain_id):
+    pssm = Pssm()
+    header = file_.readline().split()
+    for line in file_:
+        data = line.split()
+        record = {header[i]: data[i] for i in range(len(header))}
+        residue_number = int(record['pdbresi'])
+        residue_name = AA_codes_1to3[record['pdbresn']]
+        residue = Residue(residue_number, residue_name, chain_id)
+        for code in AA_codes_pssm_ordered:
+            letter = AA_codes_3to1[code]
+            value = float(record[letter])
+
+            pssm.set_amino_acid_value(residue, code, value)
+
+        pssm.set_residue_value(residue, float(record['IC']))
+    return pssm
