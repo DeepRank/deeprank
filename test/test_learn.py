83ff38b0d342a686eece7ed266547797f19cf26e
Coos Baakman <coos.baakman@radboudumc.nl>
2021-06-04 13:19:36 +0000

    fix the test

--- test/test_learn.py
+++ test/test_learn.py
@@ -57,7 +57,7 @@ def test_learn():
         dataset = DataSet(hdf5_path, grid_info=grid_info,
                           select_feature='all',
                           select_target='target1',
-                          normalize_features=True, normalize_targets=True,
+                          normalize_features=True,
                           pair_chain_feature=numpy.add,
                           dict_filter={'target1':'>=1'})
 
