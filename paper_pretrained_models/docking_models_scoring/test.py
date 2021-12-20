01c121ad106532d5b9232b759caf225e8774f419
Manon Reau <manon.reau@gmail.com>
2021-12-20 15:24:25 +0100

    Update test.py

--- paper_pretrained_models/docking_models_scoring/test.py
+++ paper_pretrained_models/docking_models_scoring/test.py
@@ -2,7 +2,7 @@ import torch
 from deeprank.learn import NeuralNet
 from model_280619 import cnn_class
 import glob
-model_data = '.best_train_model.pt'
+model_data = 'best_train_model.pt'
 
 ### Change the path to point to your own graphs
 database = glob.glob('./test/*.hdf5')
