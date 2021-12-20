24f9d2c9774870e37a99b687fa269a9afdb42d80
manonreau <manon.reau@gmail.com>
2021-12-20 18:22:12 +0100

    update dataset

--- deeprank/learn/DataSet.py
+++ deeprank/learn/DataSet.py
@@ -1168,6 +1168,7 @@ class DataSet():
         try:
             target = mol_data.get('targets/' + self.select_target)[()]
         except Exception:
+            target = None
             logger.exception(f'No target value for: {fname} - not required for the test set')
 
         # close
@@ -1217,6 +1218,7 @@ class DataSet():
         try:
             target = mol_data.get('targets/' + self.select_target)[()]
         except Exception:
+            target = None
             logger.exception(f'No target value for: {fname} - not required for the test set')
 
         # close
